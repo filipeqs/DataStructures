780c820b3a79eb84dd5cc6315e742d8301e50b3d
Filipe Silva <filipeqs@outlook.com>
2023-05-01 16:34:49 -0400

    graph functions

--- graphs.js
+++ graphs.js
@@ -1,10 +1,33 @@
 class Graph {
-    constructor() {
-        this.adjagecyList = {};
+  constructor() {
+    this.adjacecyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacecyList[vertex]) this.adjacecyList.push({ vertex: [] });
+  }
+
+  addEdge(vertextOne, vertextTwo) {
+    this.adjacecyList[vertextOne].push(vertextTwo);
+    this.adjacecyList[vertextTwo].push(vertextOne);
+  }
+
+  removeEdge(vertextOne, vertextTwo) {
+    this.adjacecyList[vertextOne] = this.adjacecyList[vertextOne].filter(
+      (v) => v !== vertextTwo
+    );
+    this.adjacecyList[vertextTwo] = this.adjacecyList[vertextTwo].filter(
+      (v) => v !== vertextOne
+    );
+  }
+
+  removeVertex(vertex) {
+    const vertices = this.adjacecyList[vertex];
+    for (let i = 0; i < vertices.length; i++) {
+      const adjacentVertext = vertices[i];
+      this.removeEdge(vertex, adjacentVertext);
     }
 
-    addVertex(vertex) {
-        if (!this.adjagecyList[vertex])
-            this.adjagecyList.push({ vertex: [] });
-    }
+    delete this.adjacecyList[vertex];
+  }
 }

3b0590a1a6fad7637c92a77bb107bd04e25d3fb4
Filipe Silva <filipeqs@outlook.com>
2023-05-04 14:35:32 -0400

    fibonachi sequence

---
+++ fibonachiSequence.js
@@ -0,0 +1,11 @@
+function fib(n, memo = []) {
+  if (memo[n]) return memo[n];
+  if (n <= 2) return 1;
+
+  const res = fib(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = res;
+
+  return res;
+}
+
+console.log(fib(100));
